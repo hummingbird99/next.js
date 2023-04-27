a0911e7fd8d0e5dc615e2e13a4b40d587b2cf666
hummingbird99 <sseoulite@gmail.com>
2023-04-27 23:59:37 +0900

    Feat: 글작성 기능

--- app/write/page.js
+++ app/write/page.js
@@ -1,8 +1,11 @@
 export default function Write() {
   return (
-    <div>
+    <div className="product_post">
       <h4>글작성</h4>
-      <form action="/api/test" method="GET">
+      <form action="/api/post/new" method="POST">
+        <input name="title" placeholder="상품명" />
+        <input name="price" placeholder="가격" />
+        <input name="description" placeholder="상품설명" />
         <button type="submit">Click</button>
       </form>
     </div>
